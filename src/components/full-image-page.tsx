b46f9bcfcd7f8fa5e00a631dd6c30dd667a04757
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-04-20 18:16:43 +0200

    full-image-page layout changes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,13 +4,13 @@ export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
         <img src={image.url} alt={image.name} className="object-contain" />
       </div>
 
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
       </div>
     </div>
   );
